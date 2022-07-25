cac14dc07682fe5b67788ab06e9a7b032856ba59
NelsonGon <gonzabato@hotmail.com>
2022-07-25 21:44:03 +0200

    Oh hello fixtures

--- test_pytest.py
+++ test_pytest.py
@@ -1,14 +1,17 @@
 import pytest 
-from our_project import hello_world
+from our_project.hello_world import HelloWorld
 
-def test_init():
-    assert isinstance(hello_world.HelloWorld(hello_text="Hello World"), hello_world.HelloWorld)
+@pytest.fixture 
+def test_obj():
+    return HelloWorld(hello_text="Hello World")
+    
+# Weird that we need to pass fixtures as arguments to other functions 
+def test_init(test_obj):
+    assert isinstance(test_obj, HelloWorld)
 
-def test_hello_text_print():
-    my_obj = hello_world.HelloWorld(hello_text="Hello World")
-    assert my_obj.print_text() == "Hello World"
+def test_hello_text_print(test_obj):
+    assert test_obj.print_text() == "Hello World"
     with pytest.raises(AssertionError) as err:
-        hello_world.HelloWorld(hello_text=420.42)
+        HelloWorld(hello_text=420.42)
         assert str(err.exception) == "hello_text must be a string not float"
 
-#TODO Use fixtures 
